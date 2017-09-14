eafe026885fa80e774bb675c5f84a9eb5c511809
Guillermo Hernández <guillehg@gmail.com>
2017-09-14 10:26:10 +0200

    Add maxwellian + peak to tests

--- test_pileup.py
+++ test_pileup.py
@@ -22,39 +22,45 @@ class PileupTest(unittest.TestCase):
     def test_pile_depile(self):
         """Test the depile reverts a piling"""
         for bin_size in [0.1, 0.02, 1]:
+            x = np.arange(0, 10, bin_size)
             for l in [0.01, 0.2, 1, 2]:
-                x = np.arange(0, 10, bin_size)
-                y = stats.gamma.pdf(x, 1.5)
-                piled = poisson_pile(y, l)
-                depiled = poisson_depile(piled, l)
+                for distribution in [stats.gamma.pdf(x, 1.5),
+                                     stats.gamma.pdf(x, 1.5) + 0.3 * stats.norm.pdf(x, loc=2, scale=0.1)]:
+                    y = distribution
+                    piled = poisson_pile(y, l)
+                    depiled = poisson_depile(piled, l)
 
-                self.assertAlmostEqual(l1(y, depiled), 0)
+                    self.assertAlmostEqual(l1(y, depiled), 0)
 
     def test_pile_methods(self):
         """Test the pile methods provide similar results, excluding the mathematically unnacurate series method"""
         for bin_size in [0.1, 0.02, 0.005]:
+            x = np.arange(0, 10, bin_size)
             for l in [0.01, 0.2, 1]:
-                x = np.arange(0, 10, bin_size)
-                y = stats.gamma.pdf(x, 1.5)
+                for distribution in [stats.gamma.pdf(x, 1.5),
+                                     stats.gamma.pdf(x, 1.5) + 0.3 * stats.norm.pdf(x, loc=2, scale=0.1)]:
+                    y = distribution
 
-                piled_fourier = poisson_pile(y, l, method="Fourier")
-                piled_fourier_c = poisson_pile(y, l, method="Fourier-C")
-                piled_fourier_series = poisson_pile(y, l, method="Fourier_Series", series_order=30)
+                    piled_fourier = poisson_pile(y, l, method="Fourier")
+                    piled_fourier_c = poisson_pile(y, l, method="Fourier-C")
+                    piled_fourier_series = poisson_pile(y, l, method="Fourier_Series", series_order=30)
 
-                self.assertAlmostEqual(l1(piled_fourier, piled_fourier_c), 0)
-                self.assertAlmostEqual(l1(piled_fourier, piled_fourier_series), 0)
+                    self.assertAlmostEqual(l1(piled_fourier, piled_fourier_c), 0)
+                    self.assertAlmostEqual(l1(piled_fourier, piled_fourier_series), 0)
 
     def test_depile_methods(self):
         """Test the depile methods provide similar results, excluding the mathematically unnacurate series method"""
         for bin_size in [0.1, 0.02, 0.005]:
             for l in [0.01, 0.2, 1]:
                 x = np.arange(0, 10, bin_size)
-                y = stats.gamma.pdf(x, 1.5)
+                for distribution in [stats.gamma.pdf(x, 1.5),
+                                     stats.gamma.pdf(x, 1.5) + 0.3 * stats.norm.pdf(x, loc=2, scale=0.1)]:
+                    y = distribution
 
-                depiled_fourier = poisson_depile(y, l, method="Fourier")
-                depiled_fourier_c = poisson_depile(y, l, method="Fourier-C")
+                    depiled_fourier = poisson_depile(y, l, method="Fourier")
+                    depiled_fourier_c = poisson_depile(y, l, method="Fourier-C")
 
-                self.assertAlmostEqual(l1(depiled_fourier, depiled_fourier_c), 0)
+                    self.assertAlmostEqual(l1(depiled_fourier, depiled_fourier_c), 0)
 
                 # TODO: Add a known pile up check (?)
 
